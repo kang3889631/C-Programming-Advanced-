f5986205bc2789768acc3bad64d692d7d31a6f22
sweng <sweng@PCM-62>
2018-12-03 15:52:38 -0800

    week1_Q3

---
+++ week2_Q3/week2_Q3/Source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+	string data;
+	getline(cin, data);
+	char a[100] = data;
+	cout << data << endl;
+	return 0;
+}
