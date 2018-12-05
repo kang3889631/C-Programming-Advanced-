fbb9756d30ab6708a90ffa389510bbe589d1cb35
shiyin weng <30838538+kang3889631@users.noreply.github.com>
2018-12-04 21:24:47 -0800

    week2 Q6

--- week2_Q5/week2_Q5/Source.cpp
+++ week2_Q5/week2_Q5/Source.cpp
@@ -2,62 +2,57 @@
 using namespace std;
 
 int main() {
-	int n = 0, A = 0, B = 0, ave = 0;
-	int data[100][4],a[100][2],b[100][2];
+	int n = 0, A = 0;
+	double id = 0, c = 0, d = 0;
+	double	a[100][2];
 	cin >> n;
 	for (int i = 0; i < n; i++) {
-		cin >> data[i][0] >> data[i][1] >> data[i][2];
-		data[i][3] = data[i][2] / data[i][1];
-	}
-	for (int i = 0; i < n; i++) {
-		ave += data[i][3];
-	}
-	ave /= n;
-	for (int i = 0; i < n; i++) {
-		if (data[i][3] > ave) {
-			a[A][0] = data[i][0];
-			a[A][1] = data[i][2];
-			A++;
-		}
-		else {
-			b[B][0] = data[i][0];
-			b[B][1] = data[i][2];
-			B++;
-		}
+		cin >> id >> c >> d;
+		a[i][0] = id;
+		a[i][1] = d / c;
 	}
 
-	cout << A << endl;
-	for (int i = 0; i < A; i++) {
-		for (int j = 0; j < A-1; j++) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n-1; j++) {
 			if (a[j][1] > a[j+1][1]) {
-				int tmp = a[j][1];
+				double tmp = a[j][1];
 				a[j][1] = a[j+1][1];
 				a[j+1][1] = tmp;
-				int id = a[j][0];
+				double id = a[j][0];
 				a[j][0] = a[j + 1][0];
 				a[j + 1][0] = id;
 			}
 		}
 	}
+	/*
+	for (int i = 0; i < n; i++) {
+		cout << a[i][0] << " " << a[i][1] << endl;
+	}
+	*/
+	double max = 0;
+	for (int i = 0; i < n-1; i++) {
+		if (max < a[i + 1][1] - a[i][1]) {
+			max = a[i + 1][1] - a[i][1];
+		}
+	}
+
+	for (int i = 0; i < n - 1; i++) {
+		if (max == a[i + 1][1] - a[i][1]) {
+			A = i + 1;
+			break;
+		}
+	}
+
+	cout << n-A << endl;
+	for (int i = A; i < n; i++) {
+		cout << a[i][0] << endl;
+	}
+	cout << A << endl;
+
 	for (int i = 0; i < A; i++) {
 		cout << a[i][0] << endl;
 	}
-	cout << B << endl;
-	for (int i = 0; i < B; i++) {
-		for (int j = 0; j < B - 1; j++) {
-			if (b[j][1] > b[j + 1][1]) {
-				int tmp = b[j][1];
-				b[j][1] = b[j + 1][1];
-				b[j + 1][1] = tmp;
-				int id = b[j][0];
-				b[j][0] = b[j + 1][0];
-				b[j + 1][0] = id;
-			}
-		}
-	}
-	for (int i = 0; i < B; i++) {
-		cout << b[i][0] << endl;
-	}
+
 	return 0;
 }
 
