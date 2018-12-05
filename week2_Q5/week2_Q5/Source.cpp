e59ee77c669db9563a24a9a432942422b8122ac2
shiyin weng <30838538+kang3889631@users.noreply.github.com>
2018-12-04 19:12:22 -0800

    update

--- week2_Q5/week2_Q5/Source.cpp
+++ week2_Q5/week2_Q5/Source.cpp
@@ -3,42 +3,54 @@ using namespace std;
 
 int main() {
 	int n = 0, A = 0, B = 0;
-	int data[100][3],a[100][3],b[100][3];
+	int data[100][3],a[100][2],b[100][2];
 	cin >> n;
 	for (int i = 0; i < n; i++) {
 		cin >> data[i][0] >> data[i][1] >> data[i][2];
-	}
-	for (int i = 0; i < n; i++) {
-		cout << "data[i][2]=" << data[i][2] << endl;
-		cout << "data[i][1]=" << data[i][1] << endl;
+
 		if (data[i][2] > data[i][1] * 100) {
 			a[A][0] = data[i][0];
-			a[A][0] = data[i][0];
-			a[A][0] = data[i][0];
+			a[A][1] = data[i][2];
 			A++;
 		}
 		else {
 			b[B][0] = data[i][0];
-			b[B][1] = data[i][1];
-			b[B][2] = data[i][2];
+			b[B][1] = data[i][2];
 			B++;
 		}
 	}
-
+	cout << A << endl;
 	for (int i = 0; i < A; i++) {
-		for (int j = i + 1; j < A; j++) {
-			if (a[i][2] < a[j][2]) {
-				for (int k = 0; k < 3; k++) {
-					int tmp = a[i][k];
-					a[i][k] = a[j][k];
-					a[j][k] = tmp;
-				}
+		for (int j = 0; j < A-1; j++) {
+			if (a[j][1] > a[j+1][1]) {
+				int tmp = a[j][1];
+				a[j][1] = a[j+1][1];
+				a[j+1][1] = tmp;
+				int id = a[j][0];
+				a[j][0] = a[j + 1][0];
+				a[j + 1][0] = id;
 			}
 		}
 	}
-
 	for (int i = 0; i < A; i++) {
-		cout << a[i][2] << endl;
+		cout << a[i][0] << endl;
+	}
+	cout << B << endl;
+	for (int i = 0; i < B; i++) {
+		for (int j = 0; j < B - 1; j++) {
+			if (b[j][1] > b[j + 1][1]) {
+				int tmp = b[j][1];
+				b[j][1] = b[j + 1][1];
+				b[j + 1][1] = tmp;
+				int id = b[j][0];
+				b[j][0] = b[j + 1][0];
+				b[j + 1][0] = id;
+			}
+		}
+	}
+	for (int i = 0; i < B; i++) {
+		cout << b[i][0] << endl;
 	}
 	return 0;
 }
+
