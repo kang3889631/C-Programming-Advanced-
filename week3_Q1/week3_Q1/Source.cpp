8d29da9ee4884995ff388cb27bc7880ef39cb426
sweng <sweng@PCM-62>
2018-12-05 16:00:00 -0800

    NEW

---
+++ week3_Q1/week3_Q1/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void reverse(char *ptr) {
+	if()
+}
+
+int main(){
+	char a[500];
+	cin.getline(a, 500);
+	char *ptr;
+	ptr = a;
+	reverse(ptr);
+	return 0;
+}
