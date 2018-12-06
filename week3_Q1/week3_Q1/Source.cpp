53a70d57b8fa4467733608fda471a30f1683188d
sweng <sweng@PCM-62>
2018-12-06 11:22:18 -0800

    update

--- week3_Q1/week3_Q1/Source.cpp
+++ week3_Q1/week3_Q1/Source.cpp
@@ -1,27 +1,26 @@
 #include <iostream>
 using namespace std;
+
 char a[500] = { 0 };
 int i = 0;
-int reverse() {
+
+int rev() {
 	char c = a[i];
 	i++;
 	if (c == ' ')
 		return 1;
 	else if (c != ' '&&c != '\0') {
-		reverse();
+		rev();
 		cout << c;
 	}
 	return 1;
 }
 
-
 int main() {
 	cin.getline(a, 500);
 	while (a[i] != '\0') {
-		if (reverse() == 1) {
+		if (rev() == 1)
 			cout << " ";
-		}
 	}
 	cout << endl;
-	return 0;
 }
