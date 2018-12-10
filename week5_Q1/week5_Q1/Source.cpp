6d9d998807eb96fdff76092615a00edfa970d55f
shiyin weng <30838538+kang3889631@users.noreply.github.com>
2018-12-09 16:39:46 -0800

    week5_Q1

---
+++ week5_Q1/week5_Q1/Source.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+	int a[100][100] = {}, m = 0, n = 0, k = 0;
+	cin >> k;
+	while (k-- > 0) {
+		cin >> m >> n;
+		int result = 0;
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				cin >> a[i][j];
+			}
+		}
+
+		for (int i = 0; i < m; i++) {
+			result += a[i][0];
+			result += a[i][n - 1];
+		}
+		for (int i = 1; i < n-1; i++) {
+			result += a[0][i];
+			result += a[m-1][i];
+		}
+		cout << result << endl;
+	}
+}
