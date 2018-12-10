30c5b451a35de85bf6f1cecc2b27550eefeff980
shiyin <kang3889631@gmail.com>
2018-12-10 15:34:03 -0800

    exception: aabcd fixed

--- week9_Q2/week9_Q2/Source.cpp
+++ week9_Q2/week9_Q2/Source.cpp
@@ -5,40 +5,56 @@ using namespace std;
 int main() {
 	char str[501] = { 0 };
 	int c[26] = { 0 };
+	int spare[26] = { 0 };
 	cin.getline(str, 501);
+	bool l = false;
+	//convert lower to upper
+	for (int i = 0; i < strlen(str); i++) {
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] -= 32;
+	}
+	//count all characters to c array
 	for (int i = 0; i < strlen(str); i++) {
 		if (str[i] >= 65 && str[i] <= 90)
-			c[str[i] - 65]++;
-
-		if (str[i] >= 97 && str[i] <= 122)
-			c[str[i] - 97]++;
+			c[str[i]-65]++;
 	}
+	//find out max and delete them/it
 	int max = 0;
 	for (int i = 0; i < 26; i++) {
 		if (max < c[i])
 			max = c[i];
 	}
 	for (int i = 0; i < 26; i++) {
-		if (max == c[i])
+		if (c[i]==max)
 			c[i] = 0;
 	}
+	//find out second largest amount
 	int max2 = 0;
 	for (int i = 0; i < 26; i++) {
 		if (max2 < c[i])
 			max2 = c[i];
 	}
-	int loc = 26;
+	int loc = 0;
 	for (int i = 0; i < 26; i++) {
-		if (max2 == c[i]) {
-			for (int j = 0; j < strlen(str); j++) {
-				if (str[j] == (i + 65) || str[j] == (i + 97))
-					if (j < loc)
-						loc = j;
+		if (max2 == c[i])
+			spare[loc++] = i+65;
+	}
+
+	int count = 0;
+	for (int i = 0; i < strlen(str); i++) {
+		if (l == false) {
+			for (int j = 0; j < loc; j++) {
+				if (str[i] == spare[j]) {
+					count = spare[j];
+					l = true;
+					break;
+				}
 			}
 		}
 	}
-	char a = 65 + loc;
-	char b = 97 + loc;
+	char a, b;
+	a = count;
+	b = count + 32;
 	cout << a << "+" << b << ":" << max2 << endl;
 	return 0;
 }
