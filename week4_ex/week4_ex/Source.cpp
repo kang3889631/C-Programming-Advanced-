ae47bf71cbaa994866b301c2662877d92b7344e1
shiyin weng <30838538+kang3889631@users.noreply.github.com>
2018-12-09 16:28:04 -0800

    create new

---
+++ week4_ex/week4_ex/Source.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main() {
+
+	char buffer[10] = "ABC";
+	char *pc;
+	pc = "hello";
+	cout << pc << endl;
+	pc++;
+	cout << pc << endl;
+	cout << *pc << endl;
+	pc = buffer;
+	cout << pc;
+	return 0;
+
+}
