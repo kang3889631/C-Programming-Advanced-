316af36a9c8ab09da9b3e5ff2ce72d545c636a4f
shiyin <kang3889631@gmail.com>
2018-12-10 15:57:16 -0800

    week9_Q1 update

--- week9_Q1/week9_Q1/Source.cpp
+++ week9_Q1/week9_Q1/Source.cpp
@@ -2,26 +2,36 @@
 using namespace std;
 
 int main() {
-	int m, k, temp = 0;
+	int m, k ,temp = 0;
+	int num[5] = { 30000,3000,300,30,3 };
 	bool a = false;
-	cin >> m >> k;
-	while (temp <= m) {
-		int count = 0;
-		if (m / 10000 == 3)
-			count++;
-		if ((m % 10000) / 1000 == 3)
-			count++;
-		if ((m % 1000) / 100 == 3)
-			count++;
-		if ((m % 100) / 10 == 3)
-			count++;
-		if ((m % 10) == 3)
-			count++;
-		if (count == k)
-			a = true;
-		temp += 19;
-	}
+	bool w = false;
+	bool q = false;
+	bool b = false;
+	bool s = false;
+	bool g = false;
 
+	cin >> m >> k;
+	temp = m;
+	if (temp % 19 == 0) {
+		while (temp>0) {
+			temp -= 19;
+			if (temp / 10000 == 3)
+				w = true;
+			if ((temp % 10000) / 1000 == 3)
+				q = true;
+			if ((temp % 1000) / 100 == 3)
+				b = true;
+			if ((temp % 100) / 10 == 3)
+				s = true;
+			if ((temp % 10) == 3)
+				g = true;
+			if (w + q + b + s + g == k) {
+				a = true;
+				break;
+			}
+		}
+	}
 	if (a == true)
 		cout << "YES" << endl;
 	else
