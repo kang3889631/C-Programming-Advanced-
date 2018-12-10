e3fd17c78d9eea3e69eed707f06329ebe8db493b
shiyin weng <30838538+kang3889631@users.noreply.github.com>
2018-12-09 20:21:23 -0800

    update

--- week5_Q2/week5_Q2/Source.cpp
+++ week5_Q2/week5_Q2/Source.cpp
@@ -4,47 +4,20 @@ using namespace std;
 
 
 int main() {
-	int array[100][100] = {}, row = 0, col = 0;
+	int array[100][100] = {0}, row = 0, col = 0;
 	cin >> row >> col;
 	for (int i = 0; i < row; i++) {
 		for (int j = 0; j < col; j++) {
-			cin >> array[i][j];
-		}
-	}
-	if (col >= row) {
-		for (int i = 0; i < col; i++)
-		{
-			for (int j = 0; j <= i; j++) {
-				if (j < row)
-					cout << array[j][i - j] << endl;
-			}
-		}
-		int count = col;
-		while (count < row + col - 1) {
-			for (int i = 1; i < row; i++) {
-				if (count - i < col)
-					cout << array[i][count - i] << endl;
-			}
-			count++;
-		}
-	}
-	else {
-		for (int i = 0; i < col; i++)
-		{
-			for (int j = 0; j <= i; j++) {
-				if (j < row)
-					cout << array[j][i - j] << endl;
-			}
+		cin >> array[i][j];
 		}
 	}
 
-	int count = col;
-	while (count < row + col - 1) {
-		for (int i = 0; i < row; i++) {
-			if (count - i < col)
-				cout << array[i][count - i] << endl;
+	for (int k = 0; k < row + col - 1; k++) {
+		for (int i = 0; i <= k; i++) {
+			if(i<row&&(k-i)<col)
+			//if(array[i][k - i]!=0)
+				cout << array[i][k-i] << endl;
 		}
-		count++;
 	}
 	return 0;
 }
